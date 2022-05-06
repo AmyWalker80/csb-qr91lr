0289fc829e4b6a74300aee3adf4404d915d85c52
Code Sandbox <hello@codesandbox.io>
2022-05-06 02:31:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e4e5d1ef878728e3559689c823e1e890.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27a09a4d1ff830586df0",
+    "url": "https://amywalker80.github.io/csb-qr91lr/static/css/main.fc30ce98.chunk.css"
+  },
+  {
+    "revision": "27a09a4d1ff830586df0",
+    "url": "https://amywalker80.github.io/csb-qr91lr/static/js/main.51bca49b.chunk.js"
+  },
+  {
+    "revision": "955b11301be43f513e43",
+    "url": "https://amywalker80.github.io/csb-qr91lr/static/js/runtime~main.979fd52f.js"
+  },
+  {
+    "revision": "09fc09cca39aeb28cc7e",
+    "url": "https://amywalker80.github.io/csb-qr91lr/static/js/2.28d8c36f.chunk.js"
+  },
+  {
+    "revision": "5ef35dda0cf5226f7d682277c3385d14",
+    "url": "https://amywalker80.github.io/csb-qr91lr/index.html"
+  }
+];
